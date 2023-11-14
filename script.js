f29219f68e110c6bc2dae24ad35011a02ecea217
Sanelma B <taiteen.sanelma@outlook.com>
2023-11-14 23:39:44 +0200

    Fixed bug with function return issue and cancel
    Cleaned up the core script and fixed return issue and added cancel alert for player prompt.

--- script.js
+++ script.js
@@ -15,14 +15,26 @@
 //
 //create a funtion that takes both the player input and the computer choice as parameters and returns the results of game as a string.
 //Result options:
-//You WIN!!! playersChoice beats Computers choice.
+//You WIN!!! playerInput beats Computers choice.
 //You lose! ComputersChoice beats players choice.
 //
 //rock vs paper = paper
 //rock vs scissors = rock
 //paper vs scissors = scissors
+// let rYouSure = confirm("Are you sure you don't want to play?");
+//             console.log(rYouSure);
+        
+//             if (rYouSure === true) {
+//                 alert("Ok? Yaaaaaaayyyyy! OK! Let's play!");
+//                 gameRound();
 
-askPlayAgain();
+//             } else {
+//                 alert("Yayyy! I'm happy I could change your mind! I promise it will be super duper fun!");
+//                 gameRound();
+//             }
+//          
+
+gameRound();
 
 function gameRound() {
     console.log("Let's play a game:");
@@ -32,22 +44,21 @@ function gameRound() {
     if (player === computer) {
         console.log("it's a tie");
         alert("It's a tie! " + player + " against " + computer + "!");
-        askPlayAgain();
+        return askPlayAgain();
+        
     } else if ((( player === "ROCK") && (computer === "SCISSORS"))
     || ((player === "PAPER") && (computer === "ROCK")) 
     || ((player === "SCISSORS") && (computer === "PAPER"))) {
         console.log("You WIN!");
         alert("You WIN!!! " + "\nCongradulations, " + player + " beats " + computer + "!");
-        askPlayAgain();
-        return;
+        return askPlayAgain();
+          
     } else {
     console.log("you loose");
     alert("You Loose! " + "\nSadly, " + computer + " beats " + player + ".");
-    askPlayAgain();
-    return;
-    }
+    return askPlayAgain();
+    } 
 }
-
 
 function getComputerChoice() {
     const choicesArray = ["ROCK","PAPER","SCISSORS"];
@@ -55,33 +66,40 @@ function getComputerChoice() {
     console.log(randomChoice);
     return randomChoice;
 }
+
 function getPlayersChoice() {
-    const playersChoice = prompt("Rock,Paper or Scissors?","Write down your choice").toUpperCase();
-    console.log(playersChoice);
-    if (playersChoice === "ROCK" || playersChoice === "PAPER" || playersChoice === "SCISSORS") {
-        return playersChoice;
-    }
-    else {
+    let playerInput = prompt("Rock,Paper or Scissors?","Write down your choice");
+    console.log(playerInput);
+
+    if (playerInput === null) {
+        return alert("Canceling"); 
+    }     
+    let playerChoice = playerInput.toUpperCase();
+    console.log(playerChoice);
+    
+    if (playerChoice === "ROCK" || playerChoice === "PAPER" || playerChoice === "SCISSORS") {
+        return playerChoice;
+    } else {
         console.log("Unacceptable choice, please choose again.")
         alert("Something went wrong.\nPlease write down your answer again and make sure to check your spelling.");
-        getPlayersChoice();
-        return;
-    }
-}
+        return getPlayersChoice();
+    }  
+}    
 
 function askPlayAgain() {
-    confirm("Would you like to play another round?");
-        if (confirm === true) {
-            console.log("confirmed New Game");
-            gameRound();
-            return;
-        }
-        else {
-            console.log("good bye");
-            alert("Thanks for playing with me!");
-            return;
-        }
+    console.log("would you like to play another round?");
+    let playAgain = confirm("Would you like to play another round?");
+    console.log(playAgain);
+
+    if (playAgain === true) {
+        console.log("confirmed New Game");
+        return gameRound();
     
+
+    } else {
+        console.log("good bye");
+        return alert("Thanks for playing with me!");  
+    }  
 }
 
 
