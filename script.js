e02b34b0c2a2ac8cd3fd663ad424d24c57fe38c5
Sanelma B <taiteen.sanelma@outlook.com>
2023-11-16 23:50:22 +0200

    Spellchecked script
    Used new spellcheck function to spellcheck the script file and renamed some of the functions after reading chapter clean code in Odin.

--- script.js
+++ script.js
@@ -5,27 +5,27 @@ let playerSelection;
 let playerScore;
 let computerScore;
 
-console.log(game());
+console.log(playGame());
 
-function game() {
+function playGame() { // needs to be changed to a loop once learned
     playerScore = 0;
     computerScore = 0;
     console.log("ROUND 1:");
     console.log(playRound());
-    console.log(scoreCount());
+    console.log(updateScore());
     console.log("ROUND 2:");
     console.log(playRound());
-    console.log(scoreCount());
+    console.log(updateScore());
     console.log("ROUND 3:");
     console.log(playRound());
-    console.log(scoreCount());
+    console.log(updateScore());
     console.log("ROUND 4:");
     console.log(playRound());
-    console.log(scoreCount());
+    console.log(updateScore());
     console.log("ROUND 5:");
     console.log(playRound());
-    console.log(scoreCount());
-    console.log(winner());
+    console.log(updateScore());
+    console.log(getWinner());
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -33,7 +33,7 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = getPlayerChoice();
     console.log(playerSelection);
     if (playerSelection === false) {
-    return false; //if player presses cancel
+    return false; //if player presses cancel - needs fixing
     }
     else if (playerSelection === computerSelection) {
         return ("It's a tie! " + playerSelection + " against " + computerSelection + "!");
@@ -41,7 +41,7 @@ function playRound(playerSelection, computerSelection) {
     || ((playerSelection === "Paper") && (computerSelection === "Rock")) 
     || ((playerSelection === "Scissors") && (computerSelection === "Paper"))) {
         playerScore++
-        return ("You WIN!!! " + "\nCongradulations, " + playerSelection + " beats " + computerSelection + "!");
+        return ("You WIN!!! " + "\nCongratulations, " + playerSelection + " beats " + computerSelection + "!");
     } else {
         computerScore++
         return ("You Loose! " + "\nSadly, " + computerSelection + " beats " + playerSelection + ".");
@@ -63,7 +63,7 @@ function getPlayerChoice() {
         if (playerSelection === "Rock" || "Paper" || "Scissors") {
             return playerSelection;
         }
-        else {
+        else { // this needs debugging, does not get run
             alert("Please spell check: " + playerInput + "\nPress \"ok\" to re-enter your choice");
             return getPlayerChoice();
         } 
@@ -76,11 +76,11 @@ function capitalize(string) {
     return (firstLetter.toUpperCase()+(string.substring(1)));
 }
 
-function scoreCount() {
+function updateScore() {
     return ("SCORE COUNT:\nYour score:" + playerScore + "\nThe computers score:" + computerScore );
 }
 
-function winner() {
+function getWinner() {
     console.log("player:" + playerScore);
     console.log("computer:" + computerScore);
     if (playerScore === computerScore) {
@@ -92,6 +92,8 @@ function winner() {
     }
 }
 
-function isGameOver() {
+function isGameOver() { // add once game is looped
     return playerScore === 5 || computerScore === 5
 }
+
+// Created by Sanelma Brown, November 16th 2023
