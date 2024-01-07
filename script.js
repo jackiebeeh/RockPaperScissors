d74ca170def1e82b04f058206dc51acd13b4b8ad
Sanelma B <92818820+jackiebeeh@users.noreply.github.com>
2024-01-07 15:09:34 +0200

    Added new js
    Created script for game play
    Restart button still needs to be fixed to work properly.

--- script.js
+++ script.js
@@ -1,7 +1,82 @@
 // //GAME
 
-// let computerSelection;
-// let playerSelection;
+let buttons = document.querySelectorAll(".btn");
+let playerScore = 0;
+let computerScore = 0;
+let gameResult = document.querySelector(".gameResult");
+let score = document.querySelector(".score");
+let results = document.querySelector(".results");
+let replayButton;
+
+let gameLength = 5;
+let i =1;
+
+
+
+for (let button of buttons) {
+    button.addEventListener("click", () => {
+        console.log(button.getAttribute("value"));
+        gameTurn(button.getAttribute("value"));
+    });
+}
+
+function gameTurn(playerChoice) {
+    if ((computerScore === gameLength) || (playerScore === gameLength)) {
+        let winner
+        if (computerScore > playerScore) {
+            winner = "Computer"
+        }
+        else {
+            winner = "Player"
+        }
+        gameResult.textContent = `The ${winner} wins!`;
+        replayButton = document.createElement("button");
+        replayButton.textContent = "Play again!";
+        results.appendChild(replayButton);
+        replayButton.addEventListener("click",  replay());
+    } else {
+        console.log(`Round:${i}`);
+        i++;
+        let computerChoice = getComputerChoice();
+        if (playerChoice === computerChoice) {
+            gameResult.textContent = `It's a tie! ${playerChoice} vs ${computerChoice}`;
+        } else if ((( playerChoice === "Rock") && (computerChoice === "Scissors"))
+        || ((playerChoice === "Paper") && (computerChoice === "Rock")) 
+        || ((playerChoice === "Scissors") && (computerChoice === "Paper"))) {
+            playerScore++
+            gameResult.textContent = "You WIN!!! " + "\nCongratulations, " + playerChoice + " beats " + computerChoice + "!";
+        } else {
+            computerScore++
+            gameResult.textContent = "You Loose! " + "\nSadly, " + computerChoice + " beats " + playerChoice + ".";
+        }
+        score.textContent = `COMPUTER: ${computerScore} PLAYER: ${playerScore}`;
+    }
+}
+
+function getComputerChoice() {
+    const choicesArray = ["Rock","Paper","Scissors"];
+    return choicesArray[Math.floor(Math.random()*choicesArray.length)];
+}
+
+function replay() {
+    console.log(`replay btn clicked`);
+    playerScore = 0;
+    computerScore = 0;
+    i = 1;
+    replayButton.remove();
+}
+
+
+
+
+
+
+
+
+
+
+// let computerChoice;
+// let playerChoice;
 // let playerScore;
 // let computerScore;
 
@@ -28,23 +103,23 @@
 //     console.log(getWinner());
 // }
 
-// function playRound(playerSelection, computerSelection) {
-//     computerSelection = getComputerChoice ();
-//     playerSelection = getPlayerChoice();
-//     console.log(playerSelection);
-//     if (playerSelection === false) {
+// function playRound(playerChoice, computerChoice) {
+//     computerChoice = getComputerChoice ();
+//     playerChoice = getPlayerChoice();
+//     console.log(playerChoice);
+//     if (playerChoice === false) {
 //     return false; //if player presses cancel - FIX ME
 //     }
-//     else if (playerSelection === computerSelection) {
-//         return ("It's a tie! " + playerSelection + " against " + computerSelection + "!");
-//     } else if ((( playerSelection === "Rock") && (computerSelection === "Scissors"))
-//     || ((playerSelection === "Paper") && (computerSelection === "Rock")) 
-//     || ((playerSelection === "Scissors") && (computerSelection === "Paper"))) {
+//     else if (playerChoice === computerChoice) {
+//         return ("It's a tie! " + playerChoice + " against " + computerChoice + "!");
+//     } else if ((( playerChoice === "Rock") && (computerChoice === "Scissors"))
+//     || ((playerChoice === "Paper") && (computerChoice === "Rock")) 
+//     || ((playerChoice === "Scissors") && (computerChoice === "Paper"))) {
 //         playerScore++
-//         return ("You WIN!!! " + "\nCongratulations, " + playerSelection + " beats " + computerSelection + "!");
+//         return ("You WIN!!! " + "\nCongratulations, " + playerChoice + " beats " + computerChoice + "!");
 //     } else {
 //         computerScore++
-//         return ("You Loose! " + "\nSadly, " + computerSelection + " beats " + playerSelection + ".");
+//         return ("You Loose! " + "\nSadly, " + computerChoice + " beats " + playerChoice + ".");
 //     } 
 // }
 
@@ -59,9 +134,9 @@
 //         console.log(alert("Canceling"));
 //         return false;
 //     } else {
-//         let playerSelection = capitalize(playerInput);
-//         if (playerSelection === "Rock" || "Paper" || "Scissors") {
-//             return playerSelection;
+//         let playerChoice = capitalize(playerInput);
+//         if (playerChoice === "Rock" || "Paper" || "Scissors") {
+//             return playerChoice;
 //         }
 //         else { // FIX ME, does not get run
 //             alert("Please spell check: " + playerInput + "\nPress \"ok\" to re-enter your choice");
