e304eeba288d8f06676764aafff364498633c14b
Sanelma B <92818820+jackiebeeh@users.noreply.github.com>
2024-01-10 19:16:07 +0200

    Fixed replay button
    Still needs work on interactive sizing to ensure usability across different platforms.

--- script.js
+++ script.js
@@ -21,6 +21,21 @@ for (let button of buttons) {
 }
 
 function gameTurn(playerChoice) {
+    console.log(`Round:${i}`);
+    i++;
+    let computerChoice = getComputerChoice();
+    if (playerChoice === computerChoice) {
+        gameResult.textContent = `It's a tie! ${playerChoice} vs ${computerChoice}`;
+    } else if ((( playerChoice === "Rock") && (computerChoice === "Scissors"))
+    || ((playerChoice === "Paper") && (computerChoice === "Rock")) 
+    || ((playerChoice === "Scissors") && (computerChoice === "Paper"))) {
+        playerScore++
+        gameResult.textContent = "You WIN!!! " + "\nCongratulations, " + playerChoice + " beats " + computerChoice + "!";
+    } else {
+        computerScore++
+        gameResult.textContent = "You Loose! " + "\nSadly, " + computerChoice + " beats " + playerChoice + ".";
+    }
+    score.textContent = `COMPUTER: ${computerScore} PLAYER: ${playerScore}`;
     if ((computerScore === gameLength) || (playerScore === gameLength)) {
         if (computerScore > playerScore) {
             winner = "Computer"
@@ -31,31 +46,14 @@ function gameTurn(playerChoice) {
         gameResult.textContent = `${winner} wins!`;
         let replayButton = document.createElement("button");
         replayButton.textContent = "Play again!";
+        replayButton.className = "replay-btn";
         results.appendChild(replayButton);
         replayButton.addEventListener("click", ()=> {
             replayButton.remove();
+            console.log(`button clicked`);
             replay();
         });
-        
-        
-    } else {
-        console.log(`Round:${i}`);
-        i++;
-        let computerChoice = getComputerChoice();
-        if (playerChoice === computerChoice) {
-            gameResult.textContent = `It's a tie! ${playerChoice} vs ${computerChoice}`;
-        } else if ((( playerChoice === "Rock") && (computerChoice === "Scissors"))
-        || ((playerChoice === "Paper") && (computerChoice === "Rock")) 
-        || ((playerChoice === "Scissors") && (computerChoice === "Paper"))) {
-            playerScore++
-            gameResult.textContent = "You WIN!!! " + "\nCongratulations, " + playerChoice + " beats " + computerChoice + "!";
-        } else {
-            computerScore++
-            gameResult.textContent = "You Loose! " + "\nSadly, " + computerChoice + " beats " + playerChoice + ".";
-        }
-        score.textContent = `COMPUTER: ${computerScore} PLAYER: ${playerScore}`;
     }
-    
 }
 
 function getComputerChoice() {
@@ -70,6 +68,7 @@ function replay() {
     computerScore = 0;
     i = 1;
     winner = false;
-    results.textContent = `Choose an option`;
+    score.textContent = `COMPUTER: ${computerScore} PLAYER: ${playerScore}`;
+    gameResult.textContent = `Choose an option`;
 }
 // // Created by JackieBeeh, January 7th 2024
